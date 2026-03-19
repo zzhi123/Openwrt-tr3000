673b42c383d481725eaef288bba2499604f31ac6
zzhi123 <67152703+zzhi123@users.noreply.github.com>
2026-03-19 22:35:53 +0800

    Disable download-ci-llvm in Rust Makefile
    Fix Rust compilation failure by disabling download-ci-llvm.

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,6 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# 修复 Rust 编译失败：强制关闭 download-ci-llvm
+sed -i 's/$(TARGET_CONFIGURE_ARGS)/--set llvm.download-ci-llvm=false \\\n\t$(TARGET_CONFIGURE_ARGS)/' feeds/packages/lang/rust/Makefile
